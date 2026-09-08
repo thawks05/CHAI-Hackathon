515bbb8c8819bf558a9e857bfb3db1cfbe552d09
THAWKS05 <timothyhawks@usf.edu>
2026-09-08 00:50:55 -0400

    completed pivot_shift

--- rtl/pivot_shift.v
+++ rtl/pivot_shift.v
@@ -1,3 +1,10 @@
-module test(input v);
-    
+module pivot_shift #(
+    parameter WIDTH = 512
+)
+(
+    input wire [WIDTH-1:0] operand,
+    input wire [$clog2(WIDTH)-1:0] shift_amount,
+    output wire [WIDTH-1:0] shift_output
+);
+    assign shift_output = (operand << shift_amount) | (operand  >> (WIDTH - shift_amount));
 endmodule
